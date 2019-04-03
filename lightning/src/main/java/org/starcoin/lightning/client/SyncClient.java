9bbe40f3a005f01dfe14c29f889d33dbe7994b1d
fanngyuan <fanngyuan@gmail.com>
2019-04-03 16:22:46 +0800

    add ignore

--- lightning/src/main/java/org/starcoin/lightning/client/SyncClient.java
+++ lightning/src/main/java/org/starcoin/lightning/client/SyncClient.java
@@ -1,8 +1,12 @@
 package org.starcoin.lightning.client;
 
 import io.grpc.Channel;
+
+import com.google.protobuf.ByteString;
+
 import org.starcoin.lightning.client.core.Invoice;
 import org.starcoin.lightning.proto.LightningGrpc;
+import org.starcoin.lightning.proto.LightningOuterClass;
 
 public class SyncClient {
 
@@ -14,5 +18,11 @@ public class SyncClient {
 
   public void addInvoice(Invoice invoice){
     LightningGrpc.LightningBlockingStub stub = LightningGrpc.newBlockingStub(channel);
+    LightningOuterClass.Invoice.Builder invoiceBuilder = LightningOuterClass.Invoice.newBuilder();
+    invoiceBuilder.setRHash(ByteString.copyFrom(invoice.getrHash()));
+    invoiceBuilder.setValue(invoice.getValue());
+    LightningOuterClass.AddInvoiceResponse response = stub.addInvoice(invoiceBuilder.build());
+    String payMentRequest = response.getPaymentRequest();
+    
   }
 }
