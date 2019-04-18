d9eaabc0975c0776b5fcf187b01e2389921d1e8d
fanngyuan <fanngyuan@gmail.com>
2019-04-18 16:22:20 +0800

    add lnd rpc support

--- web/src/renderer/sdk/lightning.ts
+++ web/src/renderer/sdk/lightning.ts
@@ -35,6 +35,58 @@ function get(api: string) {
   })
 }
 
+function post(api: string,body :string) {
+  console.log("config:", config);
+  let url = config.lndUrl + "/v1" + api;
+  return fetch(url, {
+    method: "POST",
+    mode: "cors",
+    credentials: "omit",
+    headers: {
+      "Content-Type": "application/json;charset=UTF-8",
+      "Grpc-Metadata-macaroon": config.lndMacaroon
+    },
+    body:body
+  }).then(response => {
+    console.log("request resp:", response);
+    return response.json()
+  })
+
+
 export function invoice() {
   return get("/invoices")
+}
+
+export function addInvoice(rHash:string,value:number){
+  var requestBody = { 
+    r_hash:rHash,
+    value:value,
+  }
+  return post("invoices",JSON.stringify(requestBody))
+}
+
+export function sendPayment(requestString:string){
+  var requestBody = { 
+    payment_hash_string:requestString,
+  }
+  return post("channels/transactions",JSON.stringify(requestBody))
+}
+
+export function decodePayReq(requestString:string){
+  return get("payreq/"+requestString)
+}
+
+export function settleInvoice(preimage:string){
+  var requestBody = { 
+    preimage:preimage,
+  }
+  return post("channels/transactions",JSON.stringify(requestBody))
+}
+
+export function walletBalance(){
+  return get("balance/blockchain") 
+}
+
+export function lookupInvoice(rHash:string){
+  return get("invoice/"+rHash)
 }
