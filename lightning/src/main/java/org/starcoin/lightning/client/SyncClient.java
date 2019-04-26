ac9fe689e331d7d91590e089eedc5bad2da74506
tiangong3624749 <tiangong3624749@163.com>
2019-04-26 12:02:38 +0800

    add grpc timeout.

--- lightning/src/main/java/org/starcoin/lightning/client/SyncClient.java
+++ lightning/src/main/java/org/starcoin/lightning/client/SyncClient.java
@@ -2,8 +2,8 @@ package org.starcoin.lightning.client;
 
 import com.google.protobuf.ByteString;
 import io.grpc.Channel;
-import java.nio.ByteBuffer;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
@@ -23,6 +23,8 @@ public class SyncClient {
   private Logger logger = Logger.getLogger(SyncClient.class.getName());
   private Channel channel;
 
+  private int deadlineMs = 60 * 1000;
+
   public SyncClient(Channel channel) {
     this.channel = channel;
   }
@@ -31,14 +33,17 @@ public class SyncClient {
     LightningGrpc.LightningBlockingStub stub = LightningGrpc.newBlockingStub(channel);
     LightningOuterClass.Invoice invoiceProto = invoice.toProto();
     logger.info(invoiceProto.toString());
-    LightningOuterClass.AddInvoiceResponse response = stub.addInvoice(invoiceProto);
+    LightningOuterClass.AddInvoiceResponse response =
+        stub.withDeadlineAfter(deadlineMs, TimeUnit.MILLISECONDS).addInvoice(invoiceProto);
     logger.info(response.toString());
     return AddInvoiceResponse.copyFrom(response);
   }
 
   public PaymentResponse sendPayment(Payment payment) {
     LightningGrpc.LightningBlockingStub stub = LightningGrpc.newBlockingStub(channel);
-    LightningOuterClass.SendResponse response = stub.sendPaymentSync(payment.toProto());
+    LightningOuterClass.SendResponse response =
+        stub.withDeadlineAfter(deadlineMs, TimeUnit.MILLISECONDS)
+            .sendPaymentSync(payment.toProto());
     logger.info(response.toString());
     return PaymentResponse.copyFrom(response);
   }
@@ -51,7 +56,9 @@ public class SyncClient {
     requestBuilder.setIndexOffset(offset);
     requestBuilder.setReversed(reversed);
     requestBuilder.setPendingOnly(pendingOnly);
-    ListInvoiceResponse response = stub.listInvoices(requestBuilder.build());
+    ListInvoiceResponse response =
+        stub.withDeadlineAfter(deadlineMs, TimeUnit.MILLISECONDS)
+            .listInvoices(requestBuilder.build());
     logger.info(response.toString());
     return InvoiceList.copyFrom(response);
   }
@@ -61,7 +68,9 @@ public class SyncClient {
     LightningOuterClass.PaymentHash.Builder requestBuilder =
         LightningOuterClass.PaymentHash.newBuilder();
     requestBuilder.setRHashStr(rHash);
-    LightningOuterClass.Invoice invoice = stub.lookupInvoice(requestBuilder.build());
+    LightningOuterClass.Invoice invoice =
+        stub.withDeadlineAfter(deadlineMs, TimeUnit.MILLISECONDS)
+            .lookupInvoice(requestBuilder.build());
     logger.info(invoice.toString());
     return Invoice.copyFrom(invoice);
   }
@@ -71,40 +80,52 @@ public class SyncClient {
     LightningOuterClass.PayReqString.Builder requestBuilder =
         LightningOuterClass.PayReqString.newBuilder();
     requestBuilder.setPayReq(requestString);
-    LightningOuterClass.PayReq payReq = stub.decodePayReq(requestBuilder.build());
+    LightningOuterClass.PayReq payReq =
+        stub.withDeadlineAfter(deadlineMs, TimeUnit.MILLISECONDS)
+            .decodePayReq(requestBuilder.build());
     logger.info(payReq.toString());
     return PayReq.copyFrom(payReq);
   }
 
   public List<ChannelResponse> listChannels(org.starcoin.lightning.client.core.Channel channel) {
     LightningGrpc.LightningBlockingStub stub = LightningGrpc.newBlockingStub(this.channel);
-    return stub.listChannels(channel.toProto()).getChannelsList().stream()
+    return stub.withDeadlineAfter(deadlineMs, TimeUnit.MILLISECONDS).listChannels(channel.toProto())
+        .getChannelsList().stream()
         .map(c -> new ChannelResponse(c))
         .collect(Collectors.toList());
   }
 
   public String getIdentityPubkey() {
     LightningGrpc.LightningBlockingStub stub = LightningGrpc.newBlockingStub(this.channel);
-    GetInfoResponse resp = stub.getInfo(GetInfoRequest.newBuilder().build());
+    GetInfoResponse resp =
+        stub.withDeadlineAfter(deadlineMs, TimeUnit.MILLISECONDS)
+            .getInfo(GetInfoRequest.newBuilder().build());
     return resp.getIdentityPubkey();
   }
 
   public void settleInvoice(SettleInvoiceRequest request) {
     InvoicesGrpc.InvoicesBlockingStub stub = InvoicesGrpc.newBlockingStub(this.channel);
-    logger.info(stub.settleInvoice(request.toProto()).toString());
+    logger.info(
+        stub.withDeadlineAfter(deadlineMs, TimeUnit.MILLISECONDS)
+            .settleInvoice(request.toProto())
+            .toString());
   }
 
   public void cancelInvoice(byte[] paymentHash) {
     InvoicesGrpc.InvoicesBlockingStub stub = InvoicesGrpc.newBlockingStub(this.channel);
-    InvoicesOuterClass.CancelInvoiceMsg.Builder builder = InvoicesOuterClass.CancelInvoiceMsg.newBuilder();
+    InvoicesOuterClass.CancelInvoiceMsg.Builder builder =
+        InvoicesOuterClass.CancelInvoiceMsg.newBuilder();
     builder.setPaymentHash(ByteString.copyFrom(paymentHash));
-    InvoicesOuterClass.CancelInvoiceResp resp=stub.cancelInvoice(builder.build());
+    InvoicesOuterClass.CancelInvoiceResp resp =
+        stub.withDeadlineAfter(deadlineMs, TimeUnit.MILLISECONDS).cancelInvoice(builder.build());
     logger.info(resp.toString());
   }
 
   public org.starcoin.lightning.client.core.WalletBalanceResponse walletBalance() {
     LightningGrpc.LightningBlockingStub stub = LightningGrpc.newBlockingStub(this.channel);
-    WalletBalanceResponse response = stub.walletBalance(WalletBalanceRequest.newBuilder().build());
+    WalletBalanceResponse response =
+        stub.withDeadlineAfter(deadlineMs, TimeUnit.MILLISECONDS)
+            .walletBalance(WalletBalanceRequest.newBuilder().build());
     logger.info(response.toString());
     return org.starcoin.lightning.client.core.WalletBalanceResponse.copyFrom(response);
   }
