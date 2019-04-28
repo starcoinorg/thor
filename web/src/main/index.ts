d6f63bd18e240ac876cf1b8ab2ff8c70b6f2c707
jolestar <jolestar@gmail.com>
2019-04-28 14:22:16 +0800

    use standard edit menu.

--- web/src/main/index.ts
+++ web/src/main/index.ts
@@ -26,15 +26,7 @@ const appMenu: MenuItemConstructorOptions = {
 
 const editMenu: MenuItemConstructorOptions = {
   label: "Edit",
-  submenu: [
-    {label: "Undo", role: "undo"},
-    {label: "Redo", role: "redo"},
-    {type: "separator"},
-    {label: "Cut", role: "cut"},
-    {label: "Copy", role: "copy"},
-    {label: "Paste", role: "paste"},
-    {label: "Select All", role: "selectAll"}
-  ]
+  role: "editMenu"
 };
 
 const devMenu: MenuItemConstructorOptions = {
