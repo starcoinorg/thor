9b1d9e59f9be599339ebb54ac28f7200cc8e0526
fikgol <hit.cs.lijun@gmail.com>
2019-05-08 17:31:51 +0800

    Update scripts

--- scripts/lncli.sh
+++
@@ -1,5 +0,0 @@
-lncl(){
-    local user=$1
-    shift 1
-    docker exec -i -t $user lncli --lnddir=/root/.lnd/lnd_$user  --network=simnet $ 
-}
--- scripts/macaroon.sh
+++ scripts/macaroon.sh
@@ -1,9 +1,3 @@
+########## Deprecated ###############
 #!/usr/bin/env bash
-
-echo "alice macaroon is :"
-cat /tmp/thor/lnd/lnd_alice/data/chain/bitcoin/simnet/admin.macaroon | xxd -ps -c 200 | tr -d '\n'
-echo -e '\n'
-
-echo "bob macaroon is :"
-cat /tmp/thor/lnd/lnd_bob/data/chain/bitcoin/simnet/admin.macaroon | xxd -ps -c 200 | tr -d '\n'
-echo -e '\n'
+./thor.sh macaroon
